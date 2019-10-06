5ad77e1fbf1cbc8def9f987a3e3ee3e424e4a6e4
Eugene Sun <eug.sun@gmail.com>
2019-10-05 21:43:55 -0400

    Start.

---
+++ ld-45/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Global variables")]
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
